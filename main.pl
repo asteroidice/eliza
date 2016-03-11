21471588cecea9d7766a3f4a63833a65ece5a66d
asteroidice <ryan.s.rabello@gmail.com>
2016-03-10 23:33:26 -0800

    advanced pattern matching worked

--- main.pl
+++ main.pl
@@ -1,9 +1,9 @@
 pat([bye],[bye]).
 pat([hello | _ ], [hello, there]).
 pat([i,am | Rest],[why, are, you | Rest]).
+pat(List, [tell,me,more,about,your,family]) :- member(father,List).
 pat(Anything,[tell,me,more,"."]).
 
-pat(List, [tell,me,more,about,your,family]) :- member(father,List).
 
 out([]) :- nl.
 out([Head|Tail]) :- write(Head),write(' '),out(Tail).
@@ -19,9 +19,9 @@ char_to_array(X,[X|Rest]) :- read_string(Rest).
 in(List) :- read_string(InputL),atom_chars(InputS,InputL),atomic_list_concat(List," ",InputS),write(List).
 talk(Output,UserInput) :- out(Output),in(UserInput).
 
-eliza :- rap(['Hey there, Please don\'t tell me about your problems. Also I\'m very picky, so please use quotes when you talk.']).
+eliza :- rap(['Hey there, I\'m Eliza. How are you feeling right now?']).
 
-rap([bye]) :- write("Good bye, glad to see you go.").
+rap([bye]) :- write("This has been a good session. Good bye.").
 rap(Output) :-
   talk(Output, UserInput),
   pat(UserInput, NewOutput),
