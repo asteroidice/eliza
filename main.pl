dc512fcb2be3691e7c87a31a5a9f355f217815f0
asteroidice <ryan.s.rabello@gmail.com>
2016-03-10 23:57:33 -0800

    half implemented a new function

--- main.pl
+++ main.pl
@@ -32,7 +32,13 @@ char_to_array(10,[]) :- !.
 char_to_array(-1,[]) :- !.
 char_to_array(X,[X|Rest]) :- read_string(Rest).
 
-in(List) :- read_string(InputL),atom_chars(InputS,InputL),atomic_list_concat(List," ",InputS),write(List).
+in(List) :- read_string(InputL),
+  atom_chars(InputS,InputL),
+  atomic_list_concat(InputP," ",InputS),
+  %atomic_list_concat(InputP," ",InputS),
+  %atomic_list_concat(List,".",InputP),
+  write(List).
+
 talk(Output,UserInput) :- out(Output),in(UserInput).
 
 eliza :- rap(['Hey there, I\'m Eliza. How are you feeling right now?']).
