7a1390c23e8edca261eea7507341d5d636b82c83
asteroidice <ryan.s.rabello@gmail.com>
2016-03-10 22:53:34 -0800

    broke the working version

--- booktest.pl
+++ booktest.pl
@@ -1,17 +1,25 @@
-pat([bye],[bye]).
+pat([bye | _ ],[bye]).
 pat([hello | _ ], [hello, there]).
-pat([i, am | Rest],[why, are, you | Rest]).
-pat(Anything,[tell,me,more]).
+pat(["I", am | Rest],[why, are, you | Rest]).
+pat(Anything,[tell,me,more,"."]).
 
 out([]) :- nl.
 out([Head|Tail]) :- write(Head),write(' '),out(Tail).
 
-in(List) :- read(InputL),atomic_list_concat(List," ",InputL).
+my_char_type(46,period) :- !.
+
+read_string(X) :- get0(Y), char_to_array(Y,X).
+
+char_to_array(10,[]) :- !.
+char_to_array(-1,[]) :- !.
+char_to_array(X,[X|Rest]) :- read_string(Rest).
+
+in(List) :- read_string(InputL),atom_chars(InputS,InputL),atomic_list_concat(List," ",InputS),write(List).
 talk(Output,UserInput) :- out(Output),in(UserInput).
 
-eliza :- rap(['Hello, Please tell me about your problems.']).
+eliza :- rap(['Hey there, Please don\'t tell me about your problems. Also I\'m very picky, so please use quotes when you talk.']).
 
-rap([bye]) :- write(["Good bye glad to see you go."]).
+rap([bye]) :- write("Good bye, glad to see you go.").
 rap(Output) :-
   talk(Output, UserInput),
   pat(UserInput, NewOutput),
